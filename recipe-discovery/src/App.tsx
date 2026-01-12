4539c7c69185d819e8f5b419c2a590a889428483
unw10181 <uchennanwilliams@gmail.com>
2026-01-11 20:30:43 -0500

    imported to main pg

--- recipe-discovery/src/App.tsx
+++ recipe-discovery/src/App.tsx
@@ -1,14 +1,30 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+import SearchResults from "./pages/SearchResults";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      
+      <FavoritesProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:name" element={<Category />} />
+            <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/search" element={<SearchResults />} />
+          </Routes>
+        </BrowserRouter>
+      </FavoritesProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
